68d51fd16624e836df5d6133ba3608b2c9e82234
sivashankar <sivashankarlingam21@gmail.com>
2026-04-18 11:11:53 +0530

    Fix NLTK stopwords resource not found error

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Download NLTK stopwords
+python -m nltk.downloader stopwords
+
 # Collect static files
 python manage.py collectstatic --no-input
 
